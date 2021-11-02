c77a71b2b25c9600bd9cbbfbaaeb404a7d872b73
shijie <417138306@qq.com>
2021-11-02 15:03:50 +0800

    feat: fake routes

--- src/desktop_routes.jsx
+++ src/desktop_routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 import {DesktopPath} from './scripts/constants/routes';
 import App from './App';
@@ -12,7 +12,6 @@ const DesktopRoutes = () => {
         <Switch>
           <Route path={DesktopPath.HOME} component={Home}/>
           <Route path="/" component={App}/>
-          <Link to="https://baidu.com" />
         </Switch>
       </React.Suspense>
     </Router>
--- src/scripts/constants/routes.js
+++ src/scripts/constants/routes.js
@@ -1,6 +1,17 @@
 const DesktopPath = {
   HOME: '/home',
   DashBoard: '/dashboard',
+
+}
+
+const MainMenu = {
+  Route1: '/route1',
+  Route2: '/route2',
+  Route3: '/route3',
+  Route4: '/route4',
+  Route5: '/route5',
+  Route6: '/route6',
+  Route7: '/route7',
 }
 
 export { DesktopPath };
